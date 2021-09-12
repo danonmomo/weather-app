f6f01d41925a81f5c31b482e84cc46abfc503a49
danonmomo <36220439+danonmomo@users.noreply.github.com>
2021-09-12 20:19:51 +0900

    Show current temperature

--- src/app.js
+++ src/app.js
@@ -1,3 +1,18 @@
-let apiKey = "6f3f8dd6a6b0911f5669e10299b3abae";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?id={city id}&appid=${apiKey}&units=metric`;
-console.log(apiUrl);
+function displayTemperature (response) {
+//console.log(response.data.main.temp);
+let tempElement = document.querySelector("#main-temperature");
+let cityElement = document.querySelector("#city");
+let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
+let windElement = document.querySelector("#wind");
+tempElement.innerHTML = Math.round(response.data.main.temp);
+cityElement.innerHTML = response.data.name;
+descriptionElement.innerHTML = response.data.weather[0].description;
+humidityElement.innerHTML = response.data.main.humidity;
+windElement.innerHTML = response.data.wind.speed;
+console.log(response.data);
+}
+
+let apiKey = "26cc4e1e98bc3a9df038576aea64ceb0";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Tokyo&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(displayTemperature);
