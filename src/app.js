5b5fe1d741687d331071507b7427f8bcea12c3f3
danonmomo <36220439+danonmomo@users.noreply.github.com>
2021-09-26 17:34:15 +0900

    Added weather forecast col

--- src/app.js
+++ src/app.js
@@ -33,7 +33,8 @@ descriptionElement.innerHTML = response.data.weather[0].description;
 humidityElement.innerHTML = response.data.main.humidity;
 windElement.innerHTML = response.data.wind.speed;
 dateElement.innerHTML = formatDate(response.data.dt*1000);
-iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
+iconElement.setAttribute("src",`icons/${response.data.weather[0].icon}.svg`);
+//iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 iconElement.setAttribute("alt",response.data.weather[0].description);
 //console.log(response.data);
 }
